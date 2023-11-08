a9f9948019105e3f71ea6a73dcb673e536648644
Simon Johann Skødt <simon@skodt.dk>
2023-11-08 16:13:43 +0100

    feat(`none`): complete problem
    Co-authored-by: Philip Flyvholm <PhilipFlyvholm@users.noreply.github.com>

---
+++ problems/none.py
@@ -0,0 +1,30 @@
+def check_none_problem(graph,red_keys,s,t):
+    graph = remove_red(graph, red_keys)
+    path = bfs(graph,s,t)
+    if path is None:
+        return -1
+    else:
+        return len(path)
+
+def remove_red(graph, red_keys):
+    for key in red_keys:
+        graph[key] = {}
+    return graph
+
+# Based on code from old Kattis exercise: https://github.com/PhilipFlyvholm/kattis/blob/main/Waif/Waif.py#L51
+def bfs(g, start, end):
+    queue = [(start, [start])]
+    explored = [start]
+    while len(queue) > 0:
+        (v, path) = queue.pop(0)
+        if v == end:
+            return path
+        w = g[v]
+        for (adjacentV, w) in w.items():
+            if adjacentV not in explored:
+                explored.append(adjacentV)
+                localPath = path.copy()
+                localPath.append(adjacentV)
+                queue.append((adjacentV, localPath))
+    return None
+
