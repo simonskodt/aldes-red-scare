c6440b5211460038e58ee7c8d6c1c12e5d474255
Simon Johann Skødt <simon@skodt.dk>
2023-11-08 16:08:44 +0100

    feat: add command line argument parsing and delegate problem solving
    Co-authored-by: Philip Flyvholm <PhilipFlyvholm@users.noreply.github.com>
    Co-authored-by: Lasse Faurby Klausen <Faurby@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+import parse as p
+from problems import none, some, many, few, alternate
+
+def main(argv):
+    if len(argv) == 0:
+        print(f"Provide a flag to run program\nE.g.")
+        sys.exit(2)
+
+    for i, arg in enumerate(argv):
+        print(argv[i+1])
+        if i+1 > len(argv):
+            print(f"Missing argument after flag {argv}")
+            sys.exit(2)
+            
+        if arg == '--prefix':
+            print(f"Prefix: {argv[i+1]}")
+            # for file in p.find_files(arg[i+1]):
+            #     p.parse_file(file)
+            break
+        elif arg == '--file':
+            print(f"File: {argv[i+1]}")
+            graph, red_keys,s,t = p.parse_file(argv[i+1])
+            delegate_problem(graph, red_keys,s,t)
+            break
+        elif arg == '--all':
+            print(f"All")
+            for file in p.find_all_files():
+                p.parse_file(file)
+            break
+        else:
+            print("Invalid option")
+            sys.exit(2)
+
+
+def delegate_problem(graph, red_keys, s, t):
+    # solve for None
+    print("None:", none.check_none_problem(graph, red_keys,s,t))
+
+if __name__ == "__main__":
+    sys.path.insert(1, 'problems/')
+    main(sys.argv[1:])
