9cb1d1f2b7e17be09c3a2319d4e85c2da366c346
Faurby <lasse_fk@hotmail.com>
2023-11-15 16:02:42 +0100

    feat: main now writes results to results.txt, and updated requirements.txt

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ should_visualize = False
 
 def main(argv):
     global should_visualize
+    
+    results_file = open("results.txt", "w")
+    results_file.write("Instance name, n, A, F, M, N, S\n")
+
     if len(argv) == 0:
         print(f"Provide a flag to run program\nE.g.")
         sys.exit(2)
@@ -19,34 +23,48 @@ def main(argv):
         if arg == '--visualize':
             should_visualize = True
             continue
-        
+
         if arg == '--prefix':
             print(f"Prefix: {argv[i+1]}")
             # for file in p.find_files(arg[i+1]):
             #     p.parse_file(file)
             break
         elif arg == '--file':
-            print(f"File: {argv[i+1]}")
+            file = argv[i+1]
+            print(f"File: {file}")
+
             graph, red_keys,s,t, is_directed = p.parse_file(argv[i+1])
-            delegate_problem(graph, red_keys,s,t, is_directed)
+            delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
             break
         elif arg == '--all':
             print(f"All:\n")
             for file in p.find_all_files():
                 print(f"-- {file} --")
                 graph, red_keys,s,t, is_directed = p.parse_file(file)
-                delegate_problem(graph, red_keys,s,t, is_directed)
+                delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
             break
         else:
             print("Invalid option")
             sys.exit(2)
 
-def delegate_problem(graph, red_keys, s, t, is_directed):
-    # solve for None
-    print("None:", none.check_none_problem(graph, red_keys,s,t))
-    print("Alternate:", alternate.check_alternate_problem(graph, red_keys,s,t))
-    print("Few:", few.check_few_problem(graph, red_keys,s,t))
+    results_file.close()
+
+def delegate_problem(graph, red_keys, s, t, is_directed, instance_name, results_file):
+    A = alternate.check_alternate_problem(graph, red_keys,s,t)
+    F = few.check_few_problem(graph, red_keys,s,t)
+    # M = many.check_many_problem(graph, red_keys,s,t)
+    N = none.check_none_problem(graph, red_keys,s,t)
+    # S = some.check_some_problem(graph, red_keys,s,t)
+
+    print("Alternate:", A)
+    print("Few:", F)
+    print("None:", N)
     print()
+
+    # make results.txt as .csv file for output
+
+    results_file.write(f"{instance_name}, {len(graph)}, {A}, {F}, {N}\n")
+
     if should_visualize:
         visualize(graph, red_keys, s, t, is_directed)
 
