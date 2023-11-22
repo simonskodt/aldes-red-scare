a00eb88783b1ba89fd00588730090f5f875c5d5f
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-11-22 10:20:23 +0100

    Addded --files and prints

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import parse as p
 from problems import none, some, many, few, alternate   
 import graph_visualizer as gv
+import time
 
 should_visualize = False
 
@@ -33,8 +34,15 @@ def main(argv):
             file = argv[i+1]
             print(f"File: {file}")
 
-            graph, red_keys,s,t, is_directed = p.parse_file(argv[i+1])
+            graph, red_keys,s,t, is_directed = p.parse_file(file)
             delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
+            break
+        elif arg == '--files':
+            files = argv[i+1].split(",")
+            print(f"File: {files}")
+            for file in files:
+                graph, red_keys,s,t, is_directed = p.parse_file(file)
+                delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
             break
         elif arg == '--all':
             print(f"All:\n")
@@ -49,11 +57,30 @@ def main(argv):
 
     results_file.close()
 
+def start_timer():
+    return time.time()
+
+def printTimeTaken(start_time, problem, instance_name):
+    end_time = (time.time() - start_time)
+    if end_time < 1:
+        print(f"{problem} took: {round(end_time*1000,2)}ms for {instance_name}")
+    else:
+        print(f"{problem} took: {round(end_time,2)}s for {instance_name}")
+
+# Test cases: bht,common-1-5757,common-2-5757,gnm-5000-10000-0,gnm-5000-10000-1,dodecahedron,grid-50-0,grid-50-1,grid-50-2,increase-n-500-1,increase-n-500-2,increase-n-500-3,p3,rusty-1-5757,rusty-2-5757,ski-level20-1,ski-level20-2,ski-level20-3,small-world-50-0,small-world-50-1,wall-n-10000,wall-p-10000,wall-z-10000
+
+
 def delegate_problem(graph, red_keys, s, t, is_directed, instance_name, results_file):
+    a_start_time = start_timer()
     A = alternate.check_alternate_problem(graph, red_keys,s,t)
+    printTimeTaken(a_start_time, "A", instance_name)
+    f_start_time = start_timer()
     F = few.check_few_problem(graph, red_keys,s,t)
+    printTimeTaken(f_start_time, "F", instance_name)
     # M = many.check_many_problem(graph, red_keys,s,t)
+    n_start_time = start_timer()
     N = none.check_none_problem(graph, red_keys,s,t)
+    printTimeTaken(n_start_time, "N", instance_name)
     # S = some.check_some_problem(graph, red_keys,s,t)
 
     print("Alternate:", A)
@@ -63,7 +90,7 @@ def delegate_problem(graph, red_keys, s, t, is_directed, instance_name, results_
 
     # make results.txt as .csv file for output
 
-    results_file.write(f"{instance_name}, {len(graph)}, {A}, {F}, {N}\n")
+    results_file.write(f"{instance_name}, {len(graph)}, {A}, {F}, ?, {N}, ?\n")
 
     if should_visualize:
         visualize(graph, red_keys, s, t, is_directed)
