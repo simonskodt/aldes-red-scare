95cdf1260fea4f3a98eea319cbef32d9b8723766
Simon Johann Skødt <simon@skodt.dk>
2023-11-22 15:21:46 +0100

    feat: complete augment graph

--- problems/some.py
+++ problems/some.py
@@ -1,9 +1,9 @@
 import parse as parse_util
 
-def check_some_problem(g, red_keys, s, t, is_directed):
+def check_some_problem(g, red_keys, s, t, is_directed, has_no_incoming_edges):
     if not is_directed:
         return undirected_solve(g, red_keys, s, t, is_directed)
-    elif is_DAG(g, red_keys, s, t):
+    elif is_DAG(g, has_no_incoming_edges):
         graph = augment_graph(g, red_keys, s, t)
         return bellman_ford(graph)
     
@@ -11,7 +11,7 @@ def check_some_problem(g, red_keys, s, t, is_directed):
 
 # ------------------ FORD-FULKERSON ------------------ #
 
-def undirected_solve(g,red_keys,s,t, is_directed):
+def undirected_solve(g, red_keys, s, t, is_directed):
     graph = build_base_graph(g.copy())
     for red_key in red_keys:
         graph = parse_util.add_edge_to_graph(graph, red_key, "SINK-PRIME", 2)
@@ -30,14 +30,17 @@ def augment_graph(g, red_keys, s, t):
     graph = g.copy()
     red_keys = red_keys.copy()
 
+    # Nodes have a default weight of 0, so we only need to change the weights
+    # of the red vertices.
     for key in red_keys:
         if key in graph:
             for adj in graph[key]:
-                graph[key][adj] = 1
-
+                # set outgoing edges from red vertices to -1
+                graph[key][adj] = -1
+            
     return graph
 
-def is_DAG(g):
+def is_DAG(g, has_incoming_edge):
     """
     Determines whether the given graph is a Directed Acyclic Graph (DAG).
     Inspired by psudo-implementation, found at this url:
@@ -48,8 +51,10 @@ def is_DAG(g):
     """
     graph = g.copy()
     topological_order = []
-    nodes_with_no_incoming_edge = {}
-    # graph.
+    nodes_with_no_incoming_edge = has_incoming_edge.copy()
+    # while nodes_with_no_incoming_edge:
+        # nodes_with_no_incoming_edge:dict.pop
+    
 
 def bellman_ford(graph):
     return 0
