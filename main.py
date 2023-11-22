826768e30074a9ea56ff851ca5f10a87ad753421
Faurby <lasse_fk@hotmail.com>
2023-11-22 10:21:30 +0100

    feat: add nicer print options

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import sys
+import time
 import parse as p
 from problems import none, some, many, few, alternate   
 import graph_visualizer as gv
 
 should_visualize = False
+results_file = None
+log_file = None
 
 def main(argv):
     global should_visualize
+    global results_file
+    global log_file
     
     results_file = open("results.txt", "w")
     results_file.write("Instance name, n, A, F, M, N, S\n")
+
+    log_file = open("log.txt", "w")
 
     if len(argv) == 0:
         print(f"Provide a flag to run program\nE.g.")
@@ -34,36 +41,53 @@ def main(argv):
             print(f"File: {file}")
 
             graph, red_keys,s,t, is_directed = p.parse_file(argv[i+1])
-            delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
+            delegate_problem(graph, red_keys,s,t, is_directed, file)
             break
         elif arg == '--all':
             print(f"All:\n")
             for file in p.find_all_files():
                 print(f"-- {file} --")
                 graph, red_keys,s,t, is_directed = p.parse_file(file)
-                delegate_problem(graph, red_keys,s,t, is_directed, file, results_file)
+                delegate_problem(graph, red_keys,s,t, is_directed, file)
             break
         else:
             print("Invalid option")
             sys.exit(2)
 
     results_file.close()
+    log_file.close()
 
-def delegate_problem(graph, red_keys, s, t, is_directed, instance_name, results_file):
+def delegate_problem(graph, red_keys, s, t, is_directed, instance_name):
+    start_time_A = time.time()
     A = alternate.check_alternate_problem(graph, red_keys,s,t)
+    end_time_A = time.time()
+
+    start_time_F = time.time()
     F = few.check_few_problem(graph, red_keys,s,t)
+    end_time_F = time.time()
+
     # M = many.check_many_problem(graph, red_keys,s,t)
+    start_time_N = time.time()
     N = none.check_none_problem(graph, red_keys,s,t)
+    end_time_N = time.time()
     # S = some.check_some_problem(graph, red_keys,s,t)
 
-    print("Alternate:", A)
-    print("Few:", F)
-    print("None:", N)
-    print()
+    header_for_print = f"---- Instance: {instance_name}, n: {len(graph)} ----\n"
+    a_string = f"Alternate: {A} with elapsed time: {end_time_A - start_time_A}\n"
+    f_string = f"Few: {F} with elapsed time: {end_time_F - start_time_F}\n"
+    n_string = f"None: {N} with elapsed time: {end_time_N - start_time_N}\n"
+    print_string = header_for_print + a_string + f_string + n_string + "\n"
+    result_string = f"{instance_name}, {len(graph)}, {A}, {F}, {N}\n"
 
     # make results.txt as .csv file for output
+    results_file.write(result_string)
 
-    results_file.write(f"{instance_name}, {len(graph)}, {A}, {F}, {N}\n")
+    # write prints to log
+    log_file.write(print_string)
+
+    # print in terminal
+    print(print_string)
+
 
     if should_visualize:
         visualize(graph, red_keys, s, t, is_directed)
