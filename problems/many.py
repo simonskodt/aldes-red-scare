ed21a6f928d652c3a34d065a50e059f6359ed93a
Thomas <thwr@itu.dk>
2023-11-29 00:32:31 +0100

    add longest path implementation

--- problems/many.py
+++ problems/many.py
@@ -1,4 +1,6 @@
-def check_many_problem(g, red_keys, s, t):
+import copy
+from problems import some
+def check_many_problem(g, red_keys, s, t, is_directed, has_no_incoming_edges):
     """
     Check if the given graph is able to be solved.
 
@@ -13,21 +15,16 @@ def check_many_problem(g, red_keys, s, t):
             is a DAG, -1 otherwise.
     """
     graph = g.copy()
-    if (is_DAG(graph)):
-        return longest_path(graph)
-    
-    return -1 # NP-Hard
+    if is_directed and some.is_DAG(graph, has_no_incoming_edges):
+        graph = copy.deepcopy(g)
+        return longest_path(graph, red_keys, s, t)
+    return "?" # NP-Hard
 
-def is_DAG():
-    """
-    Determines whether the given graph is a Directed Acyclic Graph (DAG).
-    Inspired by psudo-implementation, found at this url:
-    https://stackoverflow.com/questions/4168/graph-serialization/4577#4577
 
-    Returns:
-        bool: True if the graph is a DAG, False otherwise.
-    """
-    raise NotImplementedError("longest path")
-
-def longest_path(g, ):
-    raise NotImplementedError("longest path")
+def longest_path(g, red_keys, s, t):
+    graph = some.augment_graph(g, red_keys, s, t)
+    distances, _ = some.bellman_ford(graph, s)
+    dist = distances[t]
+    if t in red_keys:
+        dist -= 1
+    return -dist
